8fc1d0f90539c05db840b73dc0deeabbf09a9d5b
AkimioJR <akimio.jr@gmail.com>
2026-04-04 08:41:43 +0800

    feat: 添加 LKError 枚举及其 LocalizedError 扩展，用于处理 API 错误

---
+++ Sources/LKClient/Models/LKError.swift
@@ -0,0 +1,64 @@
+//
+//  LKError.swift
+//  SwiftLK
+//
+//  Created by 秋澪 on 2026/1/1.
+//
+
+import Foundation
+
+enum LKError: Error, Sendable {
+    static let missingRequestParameter: LKError = .apiError(code: 3)
+    static let notSignedIn: LKError = .apiError(code: 5)
+
+    case apiError(code: UInt)
+
+    case apiEndpointError(String)
+    case decodingError(Error)
+    case networkError(Error)
+    case base64DecodingError
+    case zlibDecompressionError
+    case apiEmptyDataError
+
+    case unkownError(Error)
+}
+
+// MARK: - LocalizedError
+extension LKError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .apiError(let code):
+            switch code {
+            // case 1: retunrn "" // 提交 aid 为0 导致的错误，不确定具体含义
+            case 3:
+                return "缺少请求参数"
+            case 5:
+                return "未登录"
+            // case 6: retunrn "未完成" // 可能是，不确定，从task api猜测
+            default:
+                return "API 错误 (错误码: \(code))"
+            }
+
+        case .apiEndpointError(let message):
+            return "API 地址错误: \(message)"
+
+        case .apiEmptyDataError:
+            return "服务器返回数据为空"
+
+        case .base64DecodingError:
+            return "Base64 解码失败"
+
+        case .zlibDecompressionError:
+            return "数据解压缩失败"
+
+        case .decodingError(let error):
+            return "数据解析失败: \(error.localizedDescription)"
+
+        case .networkError(let error):
+            return "网络请求失败: \(error.localizedDescription)"
+
+        case .unkownError(let error):
+            return "未知错误: \(error.localizedDescription)"
+        }
+    }
+}
