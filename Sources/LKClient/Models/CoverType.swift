20348f38ee155d074b43f87306e4368bbb666190
AkimioJR <akimio.jr@gmail.com>
2026-04-04 08:43:06 +0800

    feat: 添加 CoverType 枚举及其描述扩展，用于表示封面类型

---
+++ Sources/LKClient/Models/CoverType.swift
@@ -0,0 +1,11 @@
+//
+//  CoverType.swift
+//  SwiftLK
+//
+//  Created by 秋澪 on 2025/12/18.
+//
+
+enum CoverType: UInt8, Codable, Hashable, Sendable {
+    case landscape = 0  // 横屏
+    case portrait = 1  // 竖屏
+}
