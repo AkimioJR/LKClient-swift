40fe5c66334f4c3b6051d33c5f9d93c326b74b2a
AkimioJR <akimio.jr@gmail.com>
2026-04-04 08:41:23 +0800

    feat: 添加 LKRequest 和 LKResponse 结构体，支持 HTTP 请求和响应的编码解码

---
+++ Sources/LKClient/HTTP.swift
@@ -0,0 +1,92 @@
+//
+//  HTTP.swift
+//  SwiftLK
+//
+//  Created by 秋澪 on 2025/11/9.
+//
+
+import Foundation
+
+struct LKRequest<T: Encodable & Sendable>: Encodable, Sendable {
+    @LKBool var gz: Bool
+    @LKBool var isEncrypted: Bool
+    var client: ClientType
+    var platform: PlatformType
+    var data: T
+    var versionName: String
+    var versionCode: UInt
+    var sign: String
+
+    init(
+        data: T,
+        _ gz: Bool = false,
+        _ isEncrypted: Bool = false,
+        _ client: ClientType = .app,
+        _ platform: PlatformType = .ios,
+        _ versionName: String = "0.11.51",
+        _ versionCode: UInt = 191,
+        _ sign: String = "",
+    ) {
+        self.gz = gz
+        self.isEncrypted = isEncrypted
+        self.client = client
+        self.platform = platform
+        self.data = data
+        self.versionName = versionName
+        self.versionCode = versionCode
+        self.sign = sign
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case gz = "gz"
+        case isEncrypted = "is_encrypted"
+        case client = "client"
+        case platform = "platform"
+        case data = "d"
+        case versionName = "ver_name"
+        case versionCode = "ver_code"
+        case sign = "sign"
+    }
+}
+
+struct LKResponse<T: Decodable & Sendable>: Decodable, Sendable {
+    var code: UInt
+    var time: Date
+    var data: T?
+
+    enum CodingKeys: String, CodingKey {
+        case code = "code"
+        case timeStamp = "t"
+        case data = "data"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        // 首先解析 code 和 timeStamp
+        self.code = try container.decode(UInt.self, forKey: .code)
+        let timestamp = try container.decode(UInt64.self, forKey: .timeStamp)
+        self.time = Date(timeIntervalSince1970: TimeInterval(timestamp))
+
+        // 根据 code 处理不同情况
+        switch self.code {
+        case 0:  // 成功，解析 data 字段
+            if T.self != EmptyResponse.self {
+                self.data = try container.decode(T.self, forKey: .data)
+            }
+
+        default:  // API 错误，不解析 data
+            self.data = nil
+            throw LKError.apiError(code: self.code)
+        }
+    }
+
+    // func encode(to encoder: Encoder) throws {
+    //     var container = encoder.container(keyedBy: CodingKeys.self)
+    //     try container.encode(code, forKey: .code)
+    //     try container.encode(timeStamp, forKey: .timeStamp)
+    //     if let data = data {
+    //         try container.encode(data, forKey: .data)
+    //     }
+    // }
+}
