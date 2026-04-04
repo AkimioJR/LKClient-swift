62a3835e0a1a05b53c54b785ed2ec577899b75c7
AkimioJR <akimio.jr@gmail.com>
2026-04-04 08:37:53 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "BBCode",
+  platforms: [
+    .macOS(.v14),
+    .iOS(.v17),
+    .tvOS(.v17),
+    .watchOS(.v10),
+    .visionOS(.v1),
+  ],
+  products: [
+    .library(name: "LKClient", targets: ["LKClient"])
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "LKClient",
+      dependencies: []
+    )
+  ]
+)
