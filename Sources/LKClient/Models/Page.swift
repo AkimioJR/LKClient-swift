e4f3584a414381956e7dbc29607b1af6d4639b73
AkimioJR <akimio.jr@gmail.com>
2026-04-10 19:47:51 +0800

    refactor: 重命名 Page 结构体中的 Info 为 PageInfo 以提高一致性

--- Sources/LKClient/Models/Page.swift
+++ Sources/LKClient/Models/Page.swift
@@ -5,47 +5,48 @@
 //  Created by 秋澪 on 2025/12/20.
 //
 
-public struct Page<T: Decodable & Sendable>: Decodable, Sendable {
-    public struct Info: Codable, Sendable {
-        public var count: UInt
-        public var size: UInt
-        public var currentPage: UInt
-        public var previousPage: UInt
-        public var nextPage: UInt
-        @LKBool public var hasPreviousPage: Bool
-        @LKBool public var hasNextPage: Bool
-        public var model: UInt
-        public var supportModels: [UInt]
+public struct PageInfo: Codable, Sendable {
+    public var count: UInt
+    public var size: UInt
+    public var currentPage: UInt
+    public var previousPage: UInt
+    public var nextPage: UInt
+    @LKBool public var hasPreviousPage: Bool
+    @LKBool public var hasNextPage: Bool
+    public var model: UInt
+    public var supportModels: [UInt]
 
-        public init(
-            count: UInt, size: UInt, currentPage: UInt, previousPage: UInt, nextPage: UInt,
-            hasPreviousPage: Bool, hasNextPage: Bool, model: UInt, supportModels: [UInt]
-        ) {
-            self.count = count
-            self.size = size
-            self.currentPage = currentPage
-            self.previousPage = previousPage
-            self.nextPage = nextPage
-            self.hasPreviousPage = hasPreviousPage
-            self.hasNextPage = hasNextPage
-            self.model = model
-            self.supportModels = supportModels
-        }
-
-        enum CodingKeys: String, CodingKey {
-            case count = "count"
-            case size = "size"
-            case currentPage = "cur"
-            case previousPage = "prev"
-            case nextPage = "next"
-            case hasPreviousPage = "has_prev"
-            case hasNextPage = "has_next"
-            case model = "model"
-            case supportModels = "support_model"
-        }
+    public init(
+        count: UInt, size: UInt, currentPage: UInt, previousPage: UInt, nextPage: UInt,
+        hasPreviousPage: Bool, hasNextPage: Bool, model: UInt, supportModels: [UInt]
+    ) {
+        self.count = count
+        self.size = size
+        self.currentPage = currentPage
+        self.previousPage = previousPage
+        self.nextPage = nextPage
+        self.hasPreviousPage = hasPreviousPage
+        self.hasNextPage = hasNextPage
+        self.model = model
+        self.supportModels = supportModels
     }
 
-    public var info: Info
+    enum CodingKeys: String, CodingKey {
+        case count = "count"
+        case size = "size"
+        case currentPage = "cur"
+        case previousPage = "prev"
+        case nextPage = "next"
+        case hasPreviousPage = "has_prev"
+        case hasNextPage = "has_next"
+        case model = "model"
+        case supportModels = "support_model"
+    }
+}
+
+public struct Page<T: Decodable & Sendable>: Decodable, Sendable {
+
+    public var info: PageInfo
     public var list: [T]
 
     enum CodingKeys: String, CodingKey {
