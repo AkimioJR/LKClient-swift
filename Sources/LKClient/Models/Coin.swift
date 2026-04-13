f30043ed5c2ef7e3e68dc65863eb949c9f9e20ca
AkimioJR <akimio.jr@gmail.com>
2026-04-13 23:38:35 +0800

    feat: 添加投币功能及相关请求结构

---
+++ Sources/LKClient/Models/Coin.swift
@@ -0,0 +1,52 @@
+public enum UseCoinType: UInt8, Codable, Sendable {
+    case payForArticle = 1  // 投币支付
+    case rewardArticle = 2  // 打赏文章
+}
+
+struct UseCoinRequest: Codable, Sendable {
+    var type: UseCoinType
+    var params: UInt
+    var number: UInt
+    var price: UInt
+    var totalPrice: UInt
+    var securityKey: String
+
+    enum CodingKeys: String, CodingKey {
+        case type = "goods_id"
+        case number = "number"
+        case params = "params"
+        case price = "price"
+        case totalPrice = "total_price"
+        case securityKey = "security_key"
+    }
+}
+
+/// MARK: - 投币相关
+extension LKClient {
+    /// 投币
+    public func useCoin(
+        type: UseCoinType,
+        params: UInt,
+        number: UInt = 1,
+        price: UInt = 1,
+    ) async throws(LKError) {
+        let totalPrice = number * price
+        self.logger.debug(
+            "正在使用投币，type: \(String(describing: type)), params: \(params), number: \(number), price: \(price), totalPrice: \(totalPrice)"
+        )
+        let req = await UseCoinRequest(
+            type: type,
+            params: params,
+            number: number,
+            price: price,
+            totalPrice: totalPrice,
+            securityKey: self.securityKey
+        )
+
+        try await self.sendRequest(
+            path: "/api/coin/use",
+            requestData: req,
+        )
+    }
+
+}
