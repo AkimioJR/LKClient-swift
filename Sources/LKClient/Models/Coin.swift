9aebdacf4dbc4abe9ddfdaeba8e93008269ef564
AkimioJR <akimio.jr@gmail.com>
2026-04-19 11:18:08 +0800

    refactor: 重命名 UseCoinType 为 CoinUsage，并更新相关代码以提高一致性

--- Sources/LKClient/Models/Coin.swift
+++ Sources/LKClient/Models/Coin.swift
@@ -1,10 +1,10 @@
-public enum UseCoinType: UInt8, Codable, Sendable {
+public enum CoinUsage: UInt8, Codable, Sendable {
     case payForArticle = 1  // 投币支付
     case rewardArticle = 2  // 打赏文章
 }
 
 struct UseCoinRequest: Codable, Sendable {
-    var type: UseCoinType
+    var usage: CoinUsage
     var params: UInt
     var number: UInt
     var price: UInt
@@ -12,7 +12,7 @@ struct UseCoinRequest: Codable, Sendable {
     var securityKey: String
 
     enum CodingKeys: String, CodingKey {
-        case type = "goods_id"
+        case usage = "goods_id"
         case number = "number"
         case params = "params"
         case price = "price"
@@ -25,17 +25,17 @@ struct UseCoinRequest: Codable, Sendable {
 extension LKClient {
     /// 使用硬币
     public func useCoin(
-        type: UseCoinType,
+        for usage: CoinUsage,
         params: UInt,
-        number: UInt = 1,
-        price: UInt = 1,
+        number: UInt,
+        price: UInt,
     ) async throws(LKError) {
         let totalPrice = number * price
         self.logger.debug(
-            "正在使用投币，type: \(String(describing: type)), params: \(params), number: \(number), price: \(price), totalPrice: \(totalPrice)"
+            "正在使用投币，usage: \(String(describing: usage)), params: \(params), number: \(number), price: \(price), totalPrice: \(totalPrice)"
         )
         let req = await UseCoinRequest(
-            type: type,
+            usage: usage,
             params: params,
             number: number,
             price: price,
@@ -48,23 +48,45 @@ extension LKClient {
             requestData: req,
         )
     }
+    public func useCoin(
+        for usage: CoinUsage,
+        params: UInt,
+        number: UInt,
+    ) async throws(LKError) {
+        try await self.useCoin(
+            for: usage,
+            params: params,
+            number: number,
+            price: 1
+        )
+    }
+    public func useCoin(
+        for usage: CoinUsage,
+        params: UInt,
+        price: UInt,
+    ) async throws(LKError) {
+        try await self.useCoin(
+            for: usage,
+            params: params,
+            number: 1,
+            price: price,
+        )
+    }
 
     /// 打赏文章
     public func rewardedArticle(for articleId: UInt, count: UInt) async throws(LKError) {
         try await self.useCoin(
-            type: .rewardArticle,
+            for: .rewardArticle,
             params: articleId,
             number: count,
-            price: 1,
         )
     }
 
     /// 投币支付
-    public func payForArticle(articleId: UInt, price: UInt) async throws(LKError) {
+    public func payArticle(for articleId: UInt, price: UInt) async throws(LKError) {
         try await self.useCoin(
-            type: .payForArticle,
+            for: .payForArticle,
             params: articleId,
-            number: 1,
             price: price,
         )
     }
