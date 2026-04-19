b786018de3450a81b692e4e064a29ffbdb33e15e
AkimioJR <akimio.jr@gmail.com>
2026-04-19 14:36:22 +0800

    feat: 添加 MedalType 和 MedalStatus 枚举，扩展 Medal 结构以支持 parentId 和 status 属性

--- Sources/LKClient/Models/Medal.swift
+++ Sources/LKClient/Models/Medal.swift
@@ -1,5 +1,41 @@
 import Foundation
 
+public enum MedalType: UInt8, Equatable, CaseIterable, Sendable, Codable, Identifiable {
+    case task = 1
+    case exchange = 2
+
+    public var id: UInt8 {
+        return self.rawValue
+    }
+
+    public var name: String {
+        switch self {
+        case .task: return "任务勋章"
+        case .exchange: return "兑换勋章"
+        }
+    }
+}
+
+public enum MedalStatus: UInt8, Equatable, CaseIterable, Sendable, Codable {
+    case unachieved = 0
+    case achieved = 1
+    case claimed = 2
+
+    public var name: String {
+        switch self {
+        case .unachieved: return "未达成"
+        case .achieved: return "已达成"
+        case .claimed: return "已领取"
+        }
+    }
+}
+
+public struct MedalGroup: Sendable, Codable, Identifiable {
+    public var id: UInt8
+    public var desc: String
+    public var items: [Medal]
+}
+
 public struct Medal: Codable, Hashable, Sendable {
     public var medalId: UInt
     public var name: String
@@ -10,6 +46,10 @@ public struct Medal: Codable, Hashable, Sendable {
     public var expiration: Date?  // 过期时间，可能为"0000-00-00 00:00:00"
     public var imgURL: String?
 
+    /// 仅在 /medal/list 接口中返回
+    public var parentId: UInt?
+    public var status: MedalStatus?
+
     public init(from decoder: any Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         self.medalId = try container.decode(UInt.self, forKey: .medalId)
@@ -19,10 +59,13 @@ public struct Medal: Codable, Hashable, Sendable {
         self._equip = try container.decode(LKBool<Bool?>.self, forKey: .equip)
         self.expiration = try container.decodeIfPresent(Date.self, forKey: .expiration)
         self.imgURL = try container.decodeIfPresent(String.self, forKey: .imgURL)
+        self.parentId = try container.decodeIfPresent(UInt.self, forKey: .parentId)
+        self.status = try container.decodeIfPresent(MedalStatus.self, forKey: .status)
     }
     public init(
         medalId: UInt, name: String, desc: String, medalsType: UInt, equip: Bool? = nil,
-        expiration: Date? = nil, imgURL: String? = nil
+        expiration: Date? = nil, imgURL: String? = nil, parentId: UInt? = nil,
+        status: MedalStatus? = nil
     ) {
         self.medalId = medalId
         self.name = name
@@ -31,6 +74,8 @@ public struct Medal: Codable, Hashable, Sendable {
         self.equip = equip
         self.expiration = expiration
         self.imgURL = imgURL
+        self.parentId = parentId
+        self.status = status
     }
 
     enum CodingKeys: String, CodingKey {
@@ -41,5 +86,7 @@ public struct Medal: Codable, Hashable, Sendable {
         case equip = "equip"
         case expiration = "expiration"
         case imgURL = "img"
+        case parentId = "parent_id"
+        case status = "status"
     }
 }
