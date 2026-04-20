00790ee4e2850d816730502d26750e0d4ebcd67d
AkimioJR <akimio.jr@gmail.com>
2026-04-20 20:25:25 +0800

    refactor: 重构 FlexibleString 协议，简化字符串转换逻辑

--- Sources/LKClient/FlexibleString.swift
+++ Sources/LKClient/FlexibleString.swift
@@ -1,72 +1,73 @@
 import Foundation
 
-public protocol FlexibleStringValue: Codable, Sendable, Hashable {
-    init(from container: SingleValueDecodingContainer) throws
-    func encode(to container: inout SingleValueEncodingContainer) throws
+public protocol FlexibleStringConvertible {
+    init(fromFlexibleString value: String?)
+    var flexibleStringValue: String? { get }
 }
 
-// 让 String 遵循协议
-extension String: FlexibleStringValue {
-    public init(from container: SingleValueDecodingContainer) throws {
-        if let string = try? container.decode(String.self) {
-            self = string
-        } else if let int = try? container.decode(Int.self) {
-            self = String(int)
-        } else {
-            let double = try container.decode(Double.self)
-            self = String(double)
-        }
+extension String: FlexibleStringConvertible {
+    public init(fromFlexibleString value: String?) {
+        self = value ?? ""
     }
 
-    public func encode(to container: inout SingleValueEncodingContainer) throws {
-        try container.encode(self)
+    public var flexibleStringValue: String? {
+        self
     }
 }
 
-// 让 String? (Optional<String>) 遵循协议
-extension Optional: FlexibleStringValue where Wrapped == String {
-    public init(from container: SingleValueDecodingContainer) throws {
-        if container.decodeNil() {
-            self = nil
-        } else if let string = try? container.decode(String.self) {
-            self = string
-        } else if let int = try? container.decode(Int.self) {
-            self = String(int)
-        } else {
-            let double = try container.decode(Double.self)
-            self = String(double)
-        }
+extension Optional: FlexibleStringConvertible where Wrapped == String {
+    public init(fromFlexibleString value: String?) {
+        self = value
     }
 
-    public func encode(to container: inout SingleValueEncodingContainer) throws {
-        switch self {
-        case .some(let string):
-            try container.encode(string)
-        case .none:
-            try container.encodeNil()
-        }
+    public var flexibleStringValue: String? {
+        self
     }
 }
 
-// 最终的属性包装器
-// 约束 Value: FlexibleStringValue
 @propertyWrapper
-public struct FlexibleString<Value: FlexibleStringValue>: Codable, Sendable, Hashable {
-    public var wrappedValue: Value
+public struct FlexibleString<T: FlexibleStringConvertible>: Codable {
+    public var wrappedValue: T
 
-    public init(wrappedValue: Value) {
+    public init(wrappedValue: T) {
         self.wrappedValue = wrappedValue
     }
 
     // 统一的 Decodable 实现（只有这一个，不会冲突）
     public init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        self.wrappedValue = try Value(from: container)
+        let stringValue: String?
+
+        // 尝试获取单值容器，如果失败（键不存在）则使用 nil
+        if let container = try? decoder.singleValueContainer() {
+            if container.decodeNil() {
+                stringValue = nil
+            } else if let strValue = try? container.decode(String.self) {
+                stringValue = strValue
+            } else if let intValue = try? container.decode(Int.self) {
+                stringValue = String(intValue)
+            } else if let doubleValue = try? container.decode(Double.self) {
+                stringValue = String(doubleValue)
+            } else {
+                stringValue = nil
+            }
+        } else {
+            stringValue = nil
+        }
+
+        self.wrappedValue = T(fromFlexibleString: stringValue)
     }
 
     // 统一的 Encodable 实现（只有这一个，不会冲突）
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
-        try wrappedValue.encode(to: &container)
+        if let strValue = self.wrappedValue.flexibleStringValue {
+            try container.encode(strValue)
+        } else {
+            try container.encodeNil()
+        }
     }
 }
+
+extension FlexibleString: Sendable where T: Sendable {}
+extension FlexibleString: Equatable where T: Equatable {}
+extension FlexibleString: Hashable where T: Hashable {}
