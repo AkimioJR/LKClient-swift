6eaaeca4ba45f07f4945a2ebeec1f253041df448
AkimioJR <akimio.jr@gmail.com>
2026-04-22 20:36:44 +0800

    feat(GenderType): 为性别类型添加中文名称支持

--- Sources/LKClient/Models/User.swift
+++ Sources/LKClient/Models/User.swift
@@ -11,6 +11,14 @@ public enum GenderType: UInt8, Codable, Hashable, Sendable {
     case unknown = 0
     case male = 1
     case female = 2
+
+    public var name: String {
+        switch self {
+        case .unknown: return "未知"
+        case .male: return "男"
+        case .female: return "女"
+        }
+    }
 }
 
 public struct LevelInfo: Codable, Hashable, Sendable {
