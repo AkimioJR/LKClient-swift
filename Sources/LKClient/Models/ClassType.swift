f206ebf51c74221657ef330b97a6180762f8f22f
AkimioJR <akimio.jr@gmail.com>
2026-04-26 19:50:20 +0800

    feat(Models): 为ClassType添加Hashable协议并优化id实现
    将ClassType实现Hashable协议以支持哈希操作
    将id属性类型改为Self以简化实现
    将description重命名为name以提高可读性

--- Sources/LKClient/Models/ClassType.swift
+++ Sources/LKClient/Models/ClassType.swift
@@ -5,16 +5,14 @@
 //  Created by 秋澪 on 2025/11/23.
 //
 
-public enum ClassType: UInt8, Codable, Sendable, CaseIterable, Identifiable {
+public enum ClassType: UInt8, Codable, Sendable, CaseIterable, Identifiable, Hashable {
     case article = 1  // 文章
     case series = 2  // 系列集合
     case themereply = 4  // 主题回复
 
-    public var id: UInt8 { self.rawValue }
-}
+    public var id: Self { self }
 
-extension ClassType: CustomStringConvertible {
-    public var description: String {
+    public var name: String {
         switch self {
         case .article:
             return "文章"
