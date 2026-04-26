0bff25206cb1ac027432183211132cdc659930a3
AkimioJR <akimio.jr@gmail.com>
2026-04-26 19:41:59 +0800

    feat(ArticleType): 添加 CaseIterable 等协议支持
    添加 CaseIterable, Identifiable 和 Hashable 协议支持，以便更好地在 SwiftUI 中使用

--- Sources/LKClient/Models/ArticleType.swift
+++ Sources/LKClient/Models/ArticleType.swift
@@ -5,9 +5,11 @@
 //  Created by 秋澪 on 2025/11/23.
 //
 
-public enum ArticleType: UInt8, Codable, Sendable {
+public enum ArticleType: UInt8, Codable, Sendable, CaseIterable, Identifiable, Hashable {
     case other = 0  // 其他
     case book = 1  // 图书
+
+    public var id: Self { self }
 }
 
 extension ArticleType: CustomStringConvertible {
