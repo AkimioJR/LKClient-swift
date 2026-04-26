8eebcb2fcea65adfde9567fef7b2ec91a0ac564c
AkimioJR <akimio.jr@gmail.com>
2026-04-26 19:38:24 +0800

    feat(ClassType): 添加 CaseIterable 和 Identifiable 协议支持

--- Sources/LKClient/Models/ClassType.swift
+++ Sources/LKClient/Models/ClassType.swift
@@ -5,10 +5,12 @@
 //  Created by 秋澪 on 2025/11/23.
 //
 
-public enum ClassType: UInt8, Codable, Sendable {
+public enum ClassType: UInt8, Codable, Sendable, CaseIterable, Identifiable {
     case article = 1  // 文章
     case series = 2  // 系列集合
     case themereply = 4  // 主题回复
+
+    public var id: UInt8 { self.rawValue }
 }
 
 extension ClassType: CustomStringConvertible {
